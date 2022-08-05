80bfc4f5984c8450a9656476e976a049a49fe741
Jeferson <jeferson_n@live.com>
2022-08-05 14:47:01 -0300

    tests: adicionando projeto de testes unitarios

---
+++ tests/repository/Projeto.Repository.UBaseRepositoryTest.pas
@@ -0,0 +1,31 @@
+unit Projeto.Repository.UBaseRepositoryTest;
+
+interface
+
+uses
+  DUnitX.TestFramework;
+
+type
+  [TestFixture]
+  TMyTestObject = class
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+  end;
+
+implementation
+
+procedure TMyTestObject.Setup;
+begin
+end;
+
+procedure TMyTestObject.TearDown;
+begin
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TMyTestObject);
+
+end.
